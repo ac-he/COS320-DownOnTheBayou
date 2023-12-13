5f37763f52d897462e8dc38a1ffde09facf4aefb
Anna Heebsh <98062893+ac-he@users.noreply.github.com>
2023-12-13 15:20:29 -0600

    sent texture to and from table correctly

--- gl-contexts/layeredDepthGLContext.ts
+++ gl-contexts/layeredDepthGLContext.ts
@@ -7,13 +7,13 @@ import {RenderObject} from "../helpers/renderObject";
 
 export class LayeredDepthGLContext extends GLContext {
 
-    private tableTexture:WebGLTexture;
-    private depthTexture:WebGLTexture;
+    private tableInTexture:WebGLTexture;
+    private tableOutTexture:WebGLTexture;
+    private depthOutTexture:WebGLTexture;
     private fb:WebGLFramebuffer;
 
     private uTableSampler:WebGLUniformLocation;
 
-    private spreadTable:vec4[][];
 
     constructor(canvas:HTMLCanvasElement) {
         super(canvas);
@@ -30,12 +30,15 @@ export class LayeredDepthGLContext extends GLContext {
         this.gl.enable(this.gl.BLEND);
         this.gl.blendFunc(this.gl.ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
 
-        this.tableTexture = this.gl.createTexture()
-        this.setupTextureBuffer(this.tableTexture);
+        this.tableInTexture = this.gl.createTexture()
+        this.setupTextureBuffer(this.tableInTexture);
+
+        this.tableOutTexture = this.gl.createTexture()
+        this.setupTextureBuffer(this.tableOutTexture);
 
         // set up depth texture
-        this.depthTexture = this.gl.createTexture();
-        this.gl.bindTexture(this.gl.TEXTURE_2D, this.depthTexture);
+        this.depthOutTexture = this.gl.createTexture();
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.depthOutTexture);
         this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.DEPTH_COMPONENT32F, this.canvas.clientWidth,
             this.canvas.clientHeight, 0, this.gl.DEPTH_COMPONENT, this.gl.FLOAT, null); //null data for now
         //you have to specify all 4 filters to be able to read back out from a depth texture
@@ -50,7 +53,7 @@ export class LayeredDepthGLContext extends GLContext {
 
         // add depth texture to frame buffer
         this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.DEPTH_ATTACHMENT, this.gl.TEXTURE_2D,
-            this.depthTexture, 0);
+            this.depthOutTexture, 0);
 
         this.gl.drawBuffers([this.gl.COLOR_ATTACHMENT0]);
     }
@@ -74,7 +77,7 @@ export class LayeredDepthGLContext extends GLContext {
         this.gl.useProgram(this.program);
         this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.fb);
         this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D,
-            this.tableTexture, 0);
+            this.tableOutTexture, 0);
         this.gl.clearColor(0.0, 0.0, 0.2, 1.0);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
@@ -89,35 +92,34 @@ export class LayeredDepthGLContext extends GLContext {
         // Part 2: Spread and accumulate
         // ----------------------------------
 
+        // convert texture to pixels
         let pixels:Uint8Array = new Uint8Array(this.canvas.clientWidth * this.canvas.clientHeight * 4);
         this.gl.readPixels(0, 0, this.canvas.clientWidth, this.canvas.clientHeight, this.gl.RGBA,
             this.gl.UNSIGNED_BYTE, pixels);
-        let table:number[] = this.spreadAndAccumulate(pixels);
 
-        // ----------------------------------
-        // Part 3: Render Geometry to texture
-        // ----------------------------------
+        // operate on pixels
+        let table:Uint8Array = this.spreadAndAccumulate(pixels);
 
+        // switch program
         this.gl.useProgram(this.secondPassProgram);
-        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null); // disable the frame buffer to draw to screen
-
         //setting background color for render to screen
         this.gl.clearColor(1, 0, 0, 1.0);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-        this.gl.enable(this.gl.DEPTH_TEST);
-
+        // convert pixels to texture
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.tableInTexture)
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.canvas.clientWidth, this.canvas.clientHeight,
+           0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, table);
         // read out of textures to set the texture uniforms for the shaders
         this.gl.activeTexture(this.gl.TEXTURE0);
-        this.gl.bindTexture(this.gl.TEXTURE_2D, this.tableTexture);
         this.gl.uniform1i(this.uTableSampler, 0);
 
+        // ----------------------------------
+        // Part 3: Render square from texture
+        // ----------------------------------
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.squareBufferId);
-        //send the local data over to this buffer on the graphics card.
-        // this is throwing an error but it works, and if I change it to something like "new Float32Array(table)"
-        //  it no longer works?? So I'm going to ignore it :)
-        // @ts-ignore
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, table, this.gl.STATIC_DRAW);
 
         // set vposition
         this.gl.vertexAttribPointer(this.vPositionSquare, 4, this.gl.FLOAT, false, 24, 0);
@@ -146,25 +148,25 @@ export class LayeredDepthGLContext extends GLContext {
         return table;
     }
 
-    protected spreadAndAccumulate(pixels:Uint8Array):number[]{
+    protected spreadAndAccumulate(pixels:Uint8Array):Uint8Array{
         let table:tableEntry[][] = this.createSpreadTable();
 
         for(let i = 0; i < this.canvas.clientWidth; i++){
             for(let j = 0; j < this.canvas.clientHeight; j++){
-                let base:number = i * j;
+                let base:number = i * this.canvas.clientHeight * 4 + j * 4;
                 table[i][j].color = new vec4(pixels[base], pixels[base + 1], pixels[base + 2], pixels[base + 3]);
             }
         }
 
-        let retArray = [];
+        let retArray:vec4[] = [];
 
         for(let i = 0; i < this.canvas.clientWidth; i++){
             for(let j = 0; j < this.canvas.clientHeight; j++){
-                retArray.push(table[i][j].flatten());
+                retArray.push(table[i][j].color);
             }
         }
 
-        return retArray;
+        return new Uint8Array(flatten(retArray));
     }
 
 }
@@ -178,12 +180,4 @@ class tableEntry {
         this.position = position;
     }
 
-    flatten():number[]{
-        let retArray = [];
-
-        retArray.push(this.color.flatten());
-        retArray.push(this.position.flatten());
-
-        return retArray;
-    }
 }
